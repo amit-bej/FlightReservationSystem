5ed17b7d83542fa8e59d22b0eff32c75f5c933b4
amit-bej <arb11459@gmail.com>
2022-05-31 16:48:01 +0530

    admin login

---
+++ src/main/java/User/AdminLoginServlet.java
@@ -0,0 +1,52 @@
+package User;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Connection.GetConnection;
+
+@WebServlet("/AdminLoginServlet")
+public class AdminLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Connection con = GetConnection.getConnection();
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userID = request.getParameter("username");
+		String password = request.getParameter("password");
+		RequestDispatcher dispatcher = null;
+		try
+		{
+			 PreparedStatement pt = con.prepareStatement("select * from OFRS.LOGIN_CREDENTIALS where USER_ID= ? and PASSWORD = ?");
+			 pt.setString(1,userID);
+			 pt.setString(2,password);
+			 
+			 ResultSet rs = pt.executeQuery();
+			 if(rs.next())
+			 {
+				 
+				 response.sendRedirect("admin/flightdetails/index.jsp");
+			 }
+			 else
+			 {
+				 
+				 response.sendRedirect("Adminlogin.jsp");
+			 }
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		}
+
+}
